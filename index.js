4da6094f071b9b436cad49622bf07b37fabe552c
mehbooblilla <mehbooblilla007@gmail.com>
2024-07-04 12:56:28 -0700

    create login and register apis

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express=require('express')
-
-
+const cors=require('cors')
+require('./db/config')
+const User=require('./db/User')
 const app=express()
+app.use(express.json())
+app.use(cors( ))
 
-app.get('/',(req,res)=>{
-    res.send('app is working')
+app.post('/register',async(req,res)=>{
+    const user=new User(req.body)
+    let result=await user.save()
+    result=result.toObject()
+    delete result.password
+    res.send(result)
+})
+app.post('/login',async(req,res)=>{
+    const user=await User.findOne(req.body).select('-password')
+    if(user){
+        res.send({
+            success:true,
+            data:user
+        })
+    }else{
+        res.send({
+        message:'User does not exist',
+        success:false
+        })
+    }
+    console.log("user",user);
+ 
 })
 
 
